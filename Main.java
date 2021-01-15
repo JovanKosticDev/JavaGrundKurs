a27151c607b3483182f6e6d26d8f29b0dff27393
JovanKostic <77285578+JovanKosticDev@users.noreply.github.com>
2021-01-15 22:42:57 +0100

    Scripts von der Lektion zu Collections

--- Main.java
+++ Main.java
@@ -1,98 +1,65 @@
-package com.example.oopchallenge;
+package com.example.collections;
 
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-        Patient patient1 = new Patient("Max", 25,
-                new Eye("Linkes Auge", "Kurzsichtig", "blau", true),
-                new Eye("Rechtes Auge", "Kurzsichtig", "Braun", false),
-                new Heart("Herz", "Normal", 65),
-                new Stomach("Magen","Sodbrennen", false),
-                new Skin("Haut", "Verbrannt", "Weiss", 50));
 
-        System.out.println("Name: " + patient1.getName());
-        System.out.println("Alter: " + patient1.getAge());
+//        String[] names = {"Peter", "Max", "Claudia", "Melanie", "Max"};
+//        String[] newNames = new String[6];
+//        for(int i = 0; i < names.length; i++){
+//            newNames[i] = names[i];
+//        }
+//        newNames[5] = "Florian";
+//        System.out.println(newNames[5]);
 
-        Scanner scanner = new Scanner(System.in);
+        ArrayList<String> names = new ArrayList<>();
+        names.add("Peter");
+        names.add("Max");
 
-        boolean shouldFinish = false;
-        while (!shouldFinish){
-            System.out.println("Wähle ein Organ: " +
-                    "\n\t1. Linkes Auge" +
-                    "\n\t2. Rechtes Auge" +
-                    "\n\t3. Herz" +
-                    "\n\t4. Magen" +
-                    "\n\t5. Haut" +
-                    "\n\t6. Beenden");
+//        System.out.println(names.get(0));
+//        System.out.println(names.size());
+//        //names.clear();
+//        System.out.println(names.size());
+//
+//        names.remove("Peter");
+//        System.out.println(names.get(0));
+//        System.out.println(names.contains("Max"));
+//        names.remove("Max");
+//        System.out.println(names.isEmpty());
 
-            int choice = scanner.nextInt();
-            switch (choice){
-                case 1:
-                    patient1.getLeftEye().getDetails();
-                    if(patient1.getLeftEye().isOpend()){
-                        System.out.println("\t\t1. Auge schliessen");
-                        if(scanner.nextInt() == 1){
-                            patient1.getLeftEye().close();
-                        }else{
-                            continue;
-                        }
-                    }else{
-                        System.out.println("\t\t1. Auge öffnen");
-                        if(scanner.nextInt() == 1){
-                            patient1.getLeftEye().open();
-                        }else{
-                            continue;
-                        }
-                    }
-                    break;
-                case 2:
-                    patient1.getRightEye().getDetails();
-                    if(patient1.getRightEye().isOpend()){
-                        System.out.println("\t\t1. Auge schliessen");
-                        if(scanner.nextInt() == 1){
-                            patient1.getRightEye().close();
-                        }else{
-                            continue;
-                        }
-                    }else{
-                        System.out.println("\t\t1. Auge öffnen");
-                        if(scanner.nextInt() == 1){
-                            patient1.getRightEye().open();
-                        }else{
-                            continue;
-                        }
-                    }
-                    break;
-                case 3:
-                    patient1.getHeart().getDetails();
-                    System.out.println("\t\t1. Herzrate ändern");
-                    if(scanner.nextInt() == 1){
-                        System.out.println("Bitte eine neue Herzrate eingeben: ");
-                        int newHeartRate = scanner.nextInt();
-                        patient1.getHeart().setRate(newHeartRate);
-                        System.out.println("Herzrate geändert zu: " + patient1.getHeart().getRate());
-                    }else{
-                        continue;
-                    }
-                    break;
-                case 4:
-                    patient1.getStomach().getDetails();
-                    System.out.println("\t\t1. Verdauen");
-                    if(scanner.nextInt() == 1){
-                        patient1.getStomach().digest();
-                    }else{
-                        continue;
-                    }
-                    break;
-                case 5:
-                    patient1.getSkin().getDetails();
-                    break;
-                default:
-                    shouldFinish = true;
-                    break;
+        //System.out.println(names.indexOf("Alex"));
 
-            }
+//        for(int i = 0; i < names.size(); i++){
+//            System.out.println(names.get(i));
+//        }
+
+        Map<String, String> emailList = new HashMap<>();
+        emailList.put("Max", "Max@gmail.com");
+        emailList.put("Lisa", "Lisa@gmail.com");
+
+        System.out.println(emailList.get("Max"));
+        System.out.println(emailList.size());
+
+        emailList.remove("Lisa");
+        System.out.println(emailList.size());
+
+        System.out.println(emailList.containsKey("Lisa"));
+        System.out.println(emailList.containsValue("Lisa@gmail.com"));
+
+        ArrayList<Student> students = new ArrayList<>();
+        students.add(new Student("Max", 10));
+        students.add(new Student("Nina", 25));
+
+        for(Student s: students){
+            System.out.println(s.getName());
         }
+
+
+
+
     }
 }
