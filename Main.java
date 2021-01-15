199d11c1aed3a322fd6765dc231016ab75c02d2f
JovanKostic <77285578+JovanKosticDev@users.noreply.github.com>
2021-01-15 22:37:26 +0100

    Scripts zu der Lektion OOP3

--- Main.java
+++ Main.java
@@ -1,16 +1,29 @@
-package com.example.oopsektion2;
+package com.example.oopsektion3;
 
 public class Main {
     public static void main(String[] args) {
+//        Car mercedes = new Car("Mercedes CL 500", 2, "schwarz", new Engine("M275 AMG", 5000));
+//        System.out.println(mercedes.getName());
+//        Engine engine = mercedes.getEngine();
+//
+//        System.out.println("Engine Model: " + mercedes.getEngine().getModel());
 
-        Bird phoenix = new Bird("Marco", "Blau", 2, true, 2);
-        System.out.println(phoenix.getName());
-        phoenix.eat("Mythical Zoan");
 
-        //System.out.println(phoenix.getWings());
-        phoenix.fly();
-        phoenix.fly(120);
+        // NULL bedeutet in Java = Gar Nichts
 
+//        Car mercedes = null;
+//        if(mercedes != null){
+//            System.out.println(mercedes.getName());
+//        } else{
+//            System.out.println("Das Objekt war null");
+//        }
+
+//        final int a = 5;
+//        a = a + 5;
+
+        final Engine cl500Engine = new Engine("M275 AMG", 5000);
+        cl500Engine.setRpm(4800);
+        System.out.println(cl500Engine.getRpm());
 
     }
 }
