b677395bc5da5a8a1f08c0f82a365695d379a56b
JovanKostic <77285578+JovanKosticDev@users.noreply.github.com>
2021-01-15 22:52:43 +0100

    Scripts zu der Lektion Singelton Pattern

---
+++ Database.java
@@ -0,0 +1,33 @@
+package com.example.singletonpattern;
+
+public class Database {
+    private String name;
+
+    private static Database instance;
+
+    public static synchronized Database getInstance(String name){
+        if(null == instance){
+            instance = new Database(name);
+        }
+        return instance;
+    }
+
+    private Database(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        String text = "Database Klasse\n" +
+                "Name: " + this.name;
+        return text;
+    }
+}
--- Main.java
+++ Main.java
@@ -1,75 +1,10 @@
-package com.example.concurrency;
+package com.example.singletonpattern;
 
 public class Main {
     public static void main(String[] args) {
-//        Thread thread = new Thread(new Runnable() {
-//            @Override
-//            public void run() {
-//                for(int i = 0; i < 5; i++){
-//                    System.out.println("Printing " + i + " in a worker thread");
-//                    try {
-//                        Thread.sleep(1000);
-//                    } catch (InterruptedException e) {
-//                        e.printStackTrace();
-//                    }
-//                }
-//            }
-//        });
-//        thread.start();
-//
-//        for(int i = 0; i < 5; i++){
-//            System.out.println("Printing " + i + " in a main thread");
-//            try {
-//                Thread.sleep(1000);
-//            } catch (InterruptedException e) {
-//                e.printStackTrace();
-//            }
-//        }
+        Database database = Database.getInstance("db_music");
 
-        int a = 2;
-        int b = 0;
+        System.out.println(database.toString());
 
-        String name = null;
-
-
-        try{
-            name.equals("Max");
-        }catch (NullPointerException e){
-            e.printStackTrace();
-            System.out.println("Name war NULL");
-        }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-   }
+    }
 }
